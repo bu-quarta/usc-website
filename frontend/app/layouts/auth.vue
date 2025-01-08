8883c464b018f7069da48a325511e0fb16df6497
Sngt25 <jeddycertifico25@gmail.com>
2025-01-09 07:30:34 +0800

    feat(ui): Add auth layout and profile page

---
+++ frontend/app/layouts/auth.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="bg-[radial-gradient(circle,rgba(234,167,51,1)_0%,rgba(178,164,88,1)_0%,rgba(30,155,184,1)_65%,rgba(101,159,138,1)_100%,rgba(0,153,203,1)_100%)] h-screen"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ frontend/app/pages/usc-management/profile.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+  import CardContent from "~/components/ui/card/CardContent.vue"
+
+  definePageMeta({
+    layout: "auth",
+  })
+
+  const profiles = ref([
+    {
+      name: "Admin",
+      color: "#463ED080",
+    },
+    {
+      name: "USC P.I.O",
+      color: "#A43DD080",
+    },
+    {
+      name: "USC IVC",
+      color: "#D0C93D80",
+    },
+    {
+      name: "USC Auditor",
+      color: "#11111180",
+    },
+  ])
+
+  const routeDic: { [key: string]: string } = {
+    admin: "admin",
+    "usc p.i.o": "usc-pio",
+    "usc ivc": "usc-ivc",
+    "usc auditor": "usc-auditor",
+  }
+
+  const navigate = async (path: string) => {
+    return navigateTo(`/usc-management/${routeDic[path.toLowerCase()]}`)
+  }
+</script>
+
+<template>
+  <div class="container px-52 flex items-center justify-center h-screen">
+    <div class="grid grid-cols-4 gap-4 h-40 shrink-0">
+      <template v-for="profile in profiles" :key="profile">
+        <Card
+          :class="`border-none hover:ring hover:ring-primary cursor-pointer bg-[${profile.color}] shadow-sm shadow-[${profile.color}] aspect-square relative`"
+          as="a"
+          @click="navigate(`${profile.name}`)"
+        >
+          <CardContent class="p-0">
+            <div class="flex gap-5 justify-end mt-8 mr-5">
+              <div class="size-10 bg-white rounded-full" />
+              <div class="size-10 bg-white rounded-full" />
+            </div>
+            <div class="rotate-90 w-max ml-[95px] -mt-2 text-white font-light select-none">)</div>
+            <h5 class="absolute -bottom-6 left-1/2 transform -translate-x-1/2 font-bold select-none">
+              {{ profile.name }}
+            </h5>
+          </CardContent>
+        </Card>
+      </template>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  * {
+    font-family: Roboto;
+  }
+</style>
